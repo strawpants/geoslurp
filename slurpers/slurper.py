69a0d00b11dd3d9ef80cb0568cb4077539f5a95a
Roelof Rietbroek (@pretslet) <r.rietje@gmail.com>
2018-02-05 22:59:46 +0100

    started structuring python package/modules

---
+++ slurpers/slurper.py
@@ -0,0 +1,28 @@
+# This file is part of geoslurp.
+# geoslurp is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+
+# geoslurp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with Frommle; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+# Author Roelof Rietbroek (roelof@geod.uni-bonn.de), 2018
+
+class Slurper():
+    """ (Abstract) Base class to which implemented dataslurper should comply to"""
+    def __init__(self):
+    def update(self):
+        """Checks for new data and download accordingly"""
+    def clear(self):  
+        """ Clears the data cache and deletes the corresponding database table """
+    def query(self):
+        """ perform and retrieve a datatype specific query """
+    def datahook(self):
+        """ Converts the downloaded files to alternative formats or enters the data in the database"""
