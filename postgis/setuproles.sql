ce1953a43c7a80a2b819a8d4a04abe9f14d2bc32
Roelof Rietbroek (@pretslet) <r.rietje@gmail.com>
2019-07-24 22:15:49 +0200

    changed  setup of postgis

--- postgis/setuproles.sql
+++
@@ -1,6 +0,0 @@
-/*This sets up an additional geoslurp (group) role which users will automatically made member of. This allows users to create there own schemes and to make queries on tables which are
- constructed by other users (unless they forcefully remove 'select' privileges themselves)*/
-CREATE ROLE geoslurp;
-GRANT CREATE ON DATABASE geoslurp TO geoslurp;
-SET postgis.gdal_enabled_drivers TO 'ENABLE_ALL';
-ALTER DATABASE geoslurp SET postgis.gdal_enabled_drivers TO 'ENABLE_ALL';
